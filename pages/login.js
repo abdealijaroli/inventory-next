245c55836c74872645149bf01bdb2b6b525dc15d
abdealijaroli <abdealiburu03@gmail.com>
2022-08-15 22:21:53 +0530

    login form dummy

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,26 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Login.module.css'
+import { useForm } from '@mantine/form';
+import { PasswordInput, TextInput, Button } from '@mantine/core';
 
 export default function Login() {
+    const form = useForm({
+        initialValues: { name: '', password: '' },
+
+        validate: {
+            name: (value) => (value.length < 2 ? 'Name must have at least 2 letters.' : null),
+            password: (value) => (value.length < 2 ? 'Password must have at least 8 characters.' : null),
+        },
+    });
+
     return (
-        <h1 className={styles.main}>
-            Login Page test
-        </h1>
-    )
+        <div className={styles.main}>
+            <form className={styles.form} onSubmit={form.onSubmit(console.log)}>
+                <TextInput label="Name" placeholder="Username" {...form.getInputProps('name')} />
+                <PasswordInput label="Password" placeholder="Password" {...form.getInputProps('password')} />
+                <Button type="submit" mt="sm">
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
 }
