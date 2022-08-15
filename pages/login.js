72c23df1b8b1bc1c6f7ea4c0530a0ac55bda45cf
Varun Tiwari <varuntiwari2001@gmail.com>
2022-08-15 21:54:26 +0530

    First commit

---
+++ pages/login.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Login() {
+    return (
+        <h1 className={styles.main}>
+            Login Page test
+        </h1>
+    )
+}
